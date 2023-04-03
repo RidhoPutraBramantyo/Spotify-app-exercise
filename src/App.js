32a66438b5eb595b4f42c6ca1324fb79e74c0a77
RidhoPutraBramantyo <126537811+RidhoPutraBramantyo@users.noreply.github.com>
2023-04-03 09:07:09 +0700

    Navbar and content
    This is about navbar and content of spotify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Router } from "react-router-dom";
+import HomePage from "./pages/Home";
+import Content from "./components/Content";
+import Content2 from "./components/Content2";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/home" element={<HomePage />}></Route>
+      <Route path="/navbar" element={<Navbar />}></Route>
+      <Route path="/content" element={<Content />}></Route>
+      <Route path="/content2" element={<Content2 />}></Route>
+    </Routes>
   );
 }
 
